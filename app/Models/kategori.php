2cc5519476d3a0e4d052f9adefa5892142d3a444
Amir_Razer <amirmahmoed003@gmail.com>
2024-08-15 18:51:11 +0700

    tambah fitur sidang

--- app/Models/kategori.php
+++ app/Models/kategori.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\DetailKategori;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class kategori extends Model
 {
--- resources/views/frontendnew/maap.blade.php
+++ resources/views/frontendnew/maap.blade.php
@@ -15,24 +15,34 @@
     <br>
 
     <div class="container">
-        <div class="">
-            <div class="col-md-12">
+        <div class="row">
+            <div class="col-md-3">
+
+                {{-- Dropdown untuk Kategori --}}
+                <label for="kategori" class="small mt-4">Silakan Pilih Kategori</label>
+                <select id="kategori" class="form-select" name="kategori">
+                    <option value="">Pilih Kategori</option>
+                    @foreach ($kategori as $kat)
+                        <option value="{{ $kat->id }}">{{ $kat->name }}</option>
+                    @endforeach
+                </select>
+
+                {{-- Dropdown untuk Detail Kategori, awalnya kosong --}}
+                <label for="detailkategori" class="small mt-4">Silakan Pilih Detail Kategori</label>
+                <select id="detailkategori" class="form-select" name="detailkategori">
+                    <option value="">Pilih Detail Kategori</option>
+                </select>
+                <div class="mt-4 overflow-auto" style="max-height: 300px">
+                    <h3>List UMKM</h3>
+                    <ul class="list-group small " id="listSpots">
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-9">
                 <div class="card">
                     <div class="card-header">Peta Umkm</div>
-                    <div class="card-body">
+                    <div class="card-body p-0">
                         <div id="map" style="height: 450px"></div>
-                        {{-- Dropdown untuk Kategori --}}
-                        <select id="kategori" name="kategori">
-                            <option value="">Pilih Kategori</option>
-                            @foreach ($kategori as $kat)
-                                <option value="{{ $kat->id }}">{{ $kat->name }}</option>
-                            @endforeach
-                        </select>
-
-                        {{-- Dropdown untuk Detail Kategori, awalnya kosong --}}
-                        <select id="detailkategori" name="detailkategori">
-                            <option value="">Pilih Detail Kategori</option>
-                        </select>
                     </div>
                 </div>
             </div>
@@ -86,9 +96,9 @@
                     "title": '{!! $value->name !!}',
                     "kategori": {{ $value->kategori_id }},
                     "detailKategori": {{ $value->detail_kategori_id }},
-                     "image": '{!! $value->image !!}',
-                     "slug": '{!! $value->slug !!}',
-                     
+                    "image": '{!! $value->image !!}',
+                    "slug": '{!! $value->slug !!}',
+
                 },
             @endforeach
         ];
@@ -106,34 +116,40 @@
         map.addControl(controlSearch);
 
         function updateKategoriMarkers(kategoriId) {
+            $('#listSpots').html('');
             markersLayer.clearLayers();
             for (i in datas) {
                 if (datas[i].kategori == kategoriId || !kategoriId) {
                     var title = datas[i].title,
-                    loc = datas[i].loc,
-                    marker = new L.Marker(new L.latLng(loc), {
-                        title: title
-                    });
-                    
+                        loc = datas[i].loc,
+                        marker = new L.Marker(new L.latLng(loc), {
+                            title: title
+                        });
+                    $('#listSpots').append('<li class="list-group-item"><a href="detail-spot/' + datas[i].slug +
+                        '"">' + title + '</a></li>');
+
                     markersLayer.addLayer(marker);
                     var destinationLatLng = datas[i].loc;
                     var popupContent =
-                                "<div class='my-2'><img src='./upload/spots/"+datas[i].image+"' class='img-fluid' width='700px'></div>" +
-                                "<div class='my-2'><strong>Nama Spot : </strong> <br>"+datas[i].title+"</div>" +
-                                "<div><a href='detail-spot/"+datas[i].slug+"' class='btn btn-outline-info'>Detail Spot</a></div>";
+                        "<div class='my-2'><img src='./upload/spots/" + datas[i].image +
+                        "' class='img-fluid' width='700px'></div>" +
+                        "<div class='my-2'><strong>Nama Spot : </strong> <br>" + datas[i].title + "</div>" +
+                        "<div><a href='detail-spot/" + datas[i].slug +
+                        "' class='btn btn-outline-info'>Detail Spot</a></div>";
 
-                            // Bind the popup to the marker
-                             marker.bindPopup(popupContent);
+                    // Bind the popup to the marker
+                    marker.bindPopup(popupContent);
 
-                            marker.addTo(map);
+                    marker.addTo(map);
 
-                }
                 }
             }
-        
+        }
+
 
         function updateDetailMarkers(detailKategoriId) {
             markersLayer.clearLayers();
+            $('#listSpots').html('');
             for (i in datas) {
                 if (datas[i].detailKategori == detailKategoriId || !detailKategoriId) {
                     var title = datas[i].title,
@@ -141,24 +157,29 @@
                         marker = new L.Marker(new L.latLng(loc), {
                             title: title
                         });
+                    $('#listSpots').append('<li class="list-group-item"><a href="detail-spot/' + datas[i].slug +
+                        '"">' + title + '</a></li>');
                     markersLayer.addLayer(marker);
-                        
+
                     markersLayer.addLayer(marker);
                     var destinationLatLng = datas[i].loc;
                     var popupContent =
-                                "<div class='my-2'><img src='./upload/spots/"+datas[i].image+"' class='img-fluid' width='700px'></div>" +
-                                "<div class='my-2'><strong>Nama Spot : </strong> <br>"+datas[i].title+"</div>" +
-                                "<div><a href='detail-spot/"+datas[i].slug+"' class='btn btn-outline-info'>Detail Spot</a></div>";
+                        "<div class='my-2'><img src='./upload/spots/" + datas[i].image +
+                        "' class='img-fluid' width='700px'></div>" +
+                        "<div class='my-2'><strong>Nama Spot : </strong> <br>" + datas[i].title + "</div>" +
+                        "<div><a href='detail-spot/" + datas[i].slug +
+                        "' class='btn btn-outline-info'>Detail Spot</a></div>";
 
-                            // Bind the popup to the marker
-                             marker.bindPopup(popupContent);
+                    // Bind the popup to the marker
+                    marker.bindPopup(popupContent);
 
-                            marker.addTo(map);
-                    
+                    marker.addTo(map);
+
                 }
+
             }
         }
-    
+
 
         // $('#kategori').change(function() {
         //     var kategoriId = $(this).val();
